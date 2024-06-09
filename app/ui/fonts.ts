dafb158e55b499c836e742ff32c0a2be692825df
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-06-09 19:53:40 -0300

    Test commit

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,8 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Playfair_Display } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({
     weight: ['400', '700'],
     subsets: ['latin'],
 });
+export const playfair = Playfair_Display({ subsets: ['latin'] });
