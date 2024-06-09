3efdae58bdecac2fcfcf9f7424b15c8e13162119
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-06-09 19:55:40 -0300

    Revert "Test commit"
    This reverts commit dafb158e55b499c836e742ff32c0a2be692825df.

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,7 @@
-import { Inter, Lusitana, Playfair_Display } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({
     weight: ['400', '700'],
     subsets: ['latin'],
 });
-export const playfair = Playfair_Display({ subsets: ['latin'] });
